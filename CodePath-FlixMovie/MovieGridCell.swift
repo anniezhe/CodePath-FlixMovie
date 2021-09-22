f836338e4d6f98da3308e2ddf15093d882b51610
Annie He <anniezhe0@gmail.com>
2021-09-21 23:42:47 -0400

    implement part 2

---
+++ CodePath-FlixMovie/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  CodePath-FlixMovie
+//
+//  Created by Annie He on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
